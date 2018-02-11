806be241b7395ee05e88cd378f4b507349a3f42f
airvip <sdqhwzb@163.com>
2018-02-11 09:53:52 +0800

    身份证编码

--- PHP/验证身份证合法性.php
+++ PHP/验证身份证合法性.php
@@ -14,7 +14,18 @@ function validation_filter_id_card($id_card){
         return false;
     }
 }
-// 计算身份证校验码，根据国家标准GB 11643-1999
+
+/**
+ * 计算身份证校验码，十八位身份证含义: 自1999年10月1日起在全国建立和实行公民身份证号码制度,根据国家标准GB 11643-1999
+ * 第十八位(校验位)数字的计算方法为：
+ * 1.将前面的身份证号码17位数分别乘以不同的系数。从第一位到第十七位的系数分别为：7 9 10 5 8 4 2 1 6 3 7 9 10 5 8 4 2
+ * 2.将这17位数字和系数相乘的结果相加。
+ * 3.用加出来和除以11，看余数是多少？
+ * 4余数只可能有0 1 2 3 4 5 6 7 8 9 10这11个数字。其分别对应的最后一位身份证的号码为1 0 X 9 8 7 6 5 4 3 2。
+ * 5.通过上面得知如果余数是2，就会在身份证的第18位数字上出现罗马数字的Ⅹ。如果余数是10，身份证的最后一位号码就是2。
+ * @param $idcard_base
+ * @return bool|mixed
+ */
 function idcard_verify_number($idcard_base){
     if(strlen($idcard_base)!=17){
         return false;
@@ -25,7 +36,7 @@ function idcard_verify_number($idcard_base){
     $verify_number_list=array('1','0','X','9','8','7','6','5','4','3','2');
     $checksum=0;
     for($i=0;$i<strlen($idcard_base);$i++){
-        $checksum += substr($idcard_base,$i,1) * $factor[$i];
+        $checksum += intval(substr($idcard_base,$i,1)) * $factor[$i];
     }
     $mod=$checksum % 11;
     $verify_number=$verify_number_list[$mod];
@@ -36,12 +47,7 @@ function idcard_15to18($idcard){
     if(strlen($idcard)!=15){
         return false;
     }else{
-        // 如果身份证顺序码是996 997 998 999，这些是为百岁以上老人的特殊编码
-        if(array_search(substr($idcard,12,3),array('996','997','998','999')) !== false){
-            $idcard=substr($idcard,0,6).'18'.substr($idcard,6,9);
-        }else{
-            $idcard=substr($idcard,0,6).'19'.substr($idcard,6,9);
-        }
+        $idcard=substr($idcard,0,6).'19'.substr($idcard,6,9);
     }
     $idcard=$idcard.idcard_verify_number($idcard);
     return $idcard;
