855385a06a471611c9599c4aefd143c3e9ba0c64
airvip <sdqhwzb@163.com>
2018-02-22 11:16:10 +0800

    readme

---
+++ PHP/myCurl.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sdqhw
+ */
+
+namespace lib;
+
+
+class myCurl
+{
+    /**
+     * 证书路径
+     */
+    protected static $certurl;
+    /**
+     * 私钥路径
+     */
+    protected static $prikey;
+    /**
+     * 私钥密码
+     * @var
+     */
+    protected static $keypwd;
+    /**
+     * 配置文件路径
+     * @param string $certurl   证书路径
+     * @param string $prikey    私钥路径
+     * @param string $keypwd    私钥密码
+     */
+    public function __construct($certurl='', $prikey='', $keypwd=''){
+        self::$certurl = $certurl;
+        self::$prikey = $prikey;
+        self::$keypwd = $keypwd;
+    }
+    /**
+     * https双向认证
+     * @param $url
+     * @param $data
+     * @return mixed
+     */
+    public static function curlPostSsl($url, $data){
+        $tuCurl = curl_init();
+        curl_setopt($tuCurl, CURLOPT_URL, $url);
+        //curl_setopt($tuCurl, CURLOPT_PORT , 443);
+        curl_setopt($tuCurl, CURLOPT_VERBOSE, 0);
+        curl_setopt($tuCurl, CURLOPT_HEADER, 0);
+        curl_setopt($tuCurl, CURLOPT_SSLVERSION, 1);
+        curl_setopt($tuCurl, CURLOPT_SSLCERT, self::$certurl);     // 证书路径
+        curl_setopt($tuCurl, CURLOPT_SSLCERTTYPE, "PEM");
+        curl_setopt($tuCurl, CURLOPT_SSLKEY, self::$prikey);       // 私钥路径
+        curl_setopt($tuCurl, CURLOPT_SSLKEYPASSWD, self::$keypwd);
+        curl_setopt($tuCurl, CURLOPT_SSLKEYTYPE, "PEM");
+        //curl_setopt($tuCurl, CURLOPT_CAINFO,$this->_certUrl);    //-----BEGIN PUBLIC KEY-----
+        curl_setopt($tuCurl, CURLOPT_POST, 1);
+        curl_setopt($tuCurl, CURLOPT_RETURNTRANSFER, 1);           //是否返回数据流
+        curl_setopt($tuCurl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($tuCurl, CURLOPT_POSTFIELDS, $data);
+        //curl_setopt($tuCurl, CURLOPT_HTTPHEADER, array("Content-Type: text/xml","SOAPAction: \"/soap/action/query\"", "Content-length: ".strlen($data)));
+        $tuData = curl_exec($tuCurl);
+        if( ! curl_errno($tuCurl)){
+            //$info = curl_getinfo($tuCurl);
+            //echo 'Took ' . $info['total_time'] . ' seconds to send a request to ' . $info['url'];
+        }
+        else{
+            echo 'Curl 错误: ' . curl_error($tuCurl);
+        }
+        curl_close($tuCurl);
+        return $tuData;
+    }
+}
--- README.md
+++ README.md
@@ -7,6 +7,21 @@
 * html_css 是一些简单实例
 
 
+### 目录结构
+
+**php**
+```
+|-- curl.php    curl以get或post方式请求数据(方法)
+|-- myCurl.php  curl以ssl进行发送数据(类)
+|-- down_res.php  下载远程文件或图片
+|-- str_fwrite.php  将字符串立刻写入日志
+|-- count_time_mem.php  统计代码用时及内存情况
+|-- log_fwrite.php  自定义到达n行写日志
+|-- id_check.php  验证身份证合法性
+```
+
+
+
 auther:**AIRVIP**
 
 [博客地址](http://diff.blog.wang)
